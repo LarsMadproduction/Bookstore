46cb68ccfea82ac6d37bf49d1252b9cbe2514290
Lars <schumacher90lars@gmail.com>
2024-08-11 00:02:53 +0200

    Finished

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function init(){
+    loadLocal();
     contentBooks();
 }
         
-
-
-
 function contentBooks(){
     let contentRef = document.getElementById('content');
     contentRef.innerHTML = "";
@@ -12,12 +10,12 @@ function contentBooks(){
          let bookname = books[bookindex].bookname;
          let author = books[bookindex].author;
          let likes = books[bookindex].likes;
-         let liked = books[bookindex].liked;
+         let heart = books[bookindex].heart;
          let price = books[bookindex].price;
          let publishedYear = books[bookindex].publishedYear;
          let genre = books[bookindex].genre;     
-         contentRef.innerHTML += contentTemplate(bookname, author, likes, liked, price, publishedYear, genre, bookindex)
-         const commentsRef = document.getElementsByClassName('comments');
+         contentRef.innerHTML += contentTemplate(bookname, author, likes, heart, price, publishedYear, genre, bookindex)
+         let commentsRef = document.getElementsByClassName('comments');
          for (let commentindex = 0; commentindex < books[bookindex].comments.length; commentindex++) {
             commentsRef[bookindex].innerHTML += `<tr>
                                                     <th>${books[bookindex].comments[commentindex].user}</th>
@@ -25,4 +23,48 @@ function contentBooks(){
                                                  </tr>`
          }
     }
+}
+
+function like(idx) {
+    const item = books[idx];
+    if (item.liked) {
+        item.likes--;
+        item.liked = false;
+        item.heart = heart[0];
+    } else {
+        item.likes++; 
+        item.liked = true;
+        item.heart = heart[1];
+    }
+    saveLocal();
+    loadLocal();
+    init();
+}
+
+function saveLocal(index) {
+    const item = books[index];
+    localStorage.setItem("books", JSON.stringify(books));
+}
+
+
+function loadLocal(idx) {
+    const item = books[idx];
+    let savedBook = localStorage.getItem('books');
+    if (savedBook) {books = JSON.parse(savedBook);
+    }
+}
+
+
+function addComment(idx) {
+    const item = books[idx];
+    let input = document.getElementById(`input(${idx})`);
+    if (input.value == 0) {
+        alert('Bitte Kommentar hinzufügen!')
+        return false;
+    }
+    books[idx].comments.push({user: 'Lars', comment: input.value});
+    input.value = "";
+
+    saveLocal();
+    init();
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,13 +1,13 @@
-function contentTemplate(bookname, author, likes, liked, price, publishedYear, genre, bookindex){
+function contentTemplate(bookname, author, likes, heart, price, publishedYear, genre, idx){
     
   return`   <div class="book_window max_widht">
             <div class="shadow"><h2>${bookname}</h2></div>
-            <div class="shadow bg"><img class="book_img" src="./img/book.png" alt=""></div>
+            <div class="shadow bg"><img class="book_img" src="./img/book.png" alt="Buch"></div>
             <div class="space_place">
-                <span>${price}€</span> 
+                <span>${price.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' })}</span> 
                 <div class="like">
                 <span>${likes}</span> 
-                <img class="heart_img" src="./img/notliked.png" alt="">
+                <img class="heart_img" src="${heart}" onclick="like(${idx})" alt="Herz">
             </div>
             </div>
             <div class="shadow">
@@ -34,8 +34,8 @@ function contentTemplate(bookname, author, likes, liked, price, publishedYear, g
             </table>
             </div>
             <div class="comment">
-                <textarea class="input" placeholder="Schreibe dein Kommentar ..." name="commentary" id=""></textarea>
-                <input class="message_img" type="image" src="./img/send_com.png" value="submit">
+                <textarea id="input(${idx})" class="input" placeholder="Schreibe dein Kommentar ..." name="commentary" id=""></textarea>
+                <input onclick="addComment(${idx})" class="message_img" type="image" src="./img/send_com.png" value="submit">
             </div>
             </div>
         `
